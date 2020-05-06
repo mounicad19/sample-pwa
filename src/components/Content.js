db35ad36b989a4d0980de42a7ec23c9b47cb7cf9
mounicad19 <49270410+mounicad19@users.noreply.github.com>
2020-05-06 21:38:35 +0530

    Create Content.js

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Content extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">React PWA Example</h5>
+                    <p className="card-text">
+                        Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text   Loren ipsum text
+                    </p>
+                    <a className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        )
+    }
+}
