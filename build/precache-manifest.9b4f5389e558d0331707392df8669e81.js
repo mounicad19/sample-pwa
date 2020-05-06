cf6bdbcc8b63ba670b94a6c821a10ce70c597c0e
mounicad19 <49270410+mounicad19@users.noreply.github.com>
2020-05-06 21:43:28 +0530

    Add files via upload

---
+++ build/precache-manifest.9b4f5389e558d0331707392df8669e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760b8dc1af50371f6dc6528dcf6c8593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98a171ca3ee7953b4927",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cad618e39c3029cf839e",
+    "url": "/static/js/2.e0ba28e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0ba28e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a171ca3ee7953b4927",
+    "url": "/static/js/main.10226e4e.chunk.js"
+  },
+  {
+    "revision": "99a1df5724a42cc4fef3",
+    "url": "/static/js/runtime-main.4a3acf61.js"
+  }
+]);
